b5a4ac4f7b95e8cc1676c8a36afe7368e5804fca
HS <sekhah@my.lanecc.edu>
2019-11-05 08:09:11 -0800

    Added bonus image.

--- app/src/main/java/sekhah/lane/threedoors/MainActivity.kt
+++ app/src/main/java/sekhah/lane/threedoors/MainActivity.kt
@@ -3,6 +3,7 @@ package sekhah.lane.threedoors
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -31,6 +32,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun launchSecondActivity(doorNumber: Int) {
+        imageView.visibility = View.INVISIBLE
         val intent = Intent(this, SecondActivity::class.java)
         intent.putExtra("doorNumber", doorNumber)
         startActivityForResult(intent, 1)
@@ -43,5 +45,7 @@ class MainActivity : AppCompatActivity() {
             textView.text = "You accepted a great prize in the form of a ${prize}. Well done!"
         else
             textView.text = "Why on earth would you reject a ${prize}? Are you really so arrogant?"
+        if (prize == "Healthy Goat")
+            imageView.visibility = View.VISIBLE
     }
 }
